03dae0599d6e3894117f2c9881f85c3e40829aa5
NikhilKumarapp <nikhilk.appincuba@gmail.com>
2026-05-18 17:32:09 +0530

    fix the deployment issue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,25 +1,25 @@
-// import React from 'react'
-// import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import './index.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-// )
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
 
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./main.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.js";
-import axios from "axios";
-axios.defaults.withCredentials = true;
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App";
+// import "./main.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/js/bootstrap.js";
+// import axios from "axios";
+// axios.defaults.withCredentials = true;
 
-export const backend_server = `http://localhost:5000`;
+// export const backend_server = `http://localhost:5000`;
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App></App>);
-// <React.StrictMode>
-// </React.StrictMode>
+// ReactDOM.createRoot(document.getElementById("root")).render(<App></App>);
+// // <React.StrictMode>
+// // </React.StrictMode>
